31c110f47d829761df62be0f11672f961fbe30cf
Aniskovich <aniskovichav@gmail.com>
2024-06-07 17:31:44 +0300

    update Appetizer

--- src/by/itstep/aniskovich/java/stage17/lunchdelivery/model/entity/dish/Appetizer.java
+++ src/by/itstep/aniskovich/java/stage17/lunchdelivery/model/entity/dish/Appetizer.java
@@ -2,6 +2,7 @@ package by.itstep.aniskovich.java.stage17.lunchdelivery.model.entity.dish;
 
 import by.itstep.aniskovich.java.stage17.lunchdelivery.model.entity.product.Product;
 
+import java.io.*;
 import java.util.List;
 
 public class Appetizer extends AbstractDish {
