bc5fa62e5fb0584f205b83000334acba4ee655b0
Aniskovich <aniskovichav@gmail.com>
2024-06-07 17:34:21 +0300

    update fields and methods DailyMenu (OCP)

--- src/by/itstep/aniskovich/java/stage17/lunchdelivery/model/entity/container/DailyMenu.java
+++ src/by/itstep/aniskovich/java/stage17/lunchdelivery/model/entity/container/DailyMenu.java
@@ -2,81 +2,45 @@ package by.itstep.aniskovich.java.stage17.lunchdelivery.model.entity.container;
 
 import by.itstep.aniskovich.java.stage17.lunchdelivery.model.entity.dish.*;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DailyMenu {
-    private final List<Salad> salads = new ArrayList<>();
-    private final List<Soup> soups = new ArrayList<>();
-    private final List<Garnish> garnishes = new ArrayList<>();
-    private final List<MainCourse> mainCourses = new ArrayList<>();
-    private final List<Appetizer> appetizers = new ArrayList<>();
-    private final List<Extra> extras = new ArrayList<>();
-    private final List<VegMainCourse>  vegMainCourses = new ArrayList<>();
+public class DailyMenu implements Serializable {
+    private static final long serialVersionUID = 4L;
 
-    public void addSalad(Salad salad) {
-        salads.add(salad);
+    private int day;
+    private List<AbstractDish> dishes = new ArrayList<>();
+
+    public DailyMenu(int day, List<AbstractDish> dishes) {
+        this.day = day;
+        this.dishes = dishes;
+    }
+    public int getDay() {
+        return day;
+    }
+    public List<AbstractDish> getDishes() {
+        return dishes;
     }
 
-    public void addSoup(Soup soup) {
-        soups.add(soup);
+    public void addDish(AbstractDish dish) {
+        dishes.add(dish);
     }
 
-    public void addGarnish(Garnish garnish) {
-        garnishes.add(garnish);
+    public void removeDish(AbstractDish dish) {
+        dishes.remove(dish);
     }
 
-    public void addMainCourse(MainCourse mainCourse) {
-        mainCourses.add(mainCourse);
+    public void setDishes(List<AbstractDish> dishes) {
+        this.dishes = dishes;
     }
-
-    public void addAppetizer(Appetizer appetizer) {
-        appetizers.add(appetizer);
-    }
-
-    public void addExtra(Extra extra) {
-        extras.add(extra);
-    }
-
-    public void addVegMainCourse(VegMainCourse vegMainCourse) {
-        vegMainCourses.add(vegMainCourse);
-    }
-
-    public List<Salad> getSalads() {
-        return salads;
-    }
-
-    public List<Soup> getSoups() {
-        return soups;
-    }
-
-    public List<Garnish> getGarnishes() {
-        return garnishes;
-    }
-
-    public List<MainCourse> getMainCourses() {
-        return mainCourses;
-    }
-
-    public List<Appetizer> getAppetizers() {
-        return appetizers;
-    }
-
-    public List<Extra> getExtras() {
-        return extras;
-    }
-
-    public List<VegMainCourse> getVegMainCourses() {
-        return vegMainCourses;
-    }
-
     public void clear() {
-        salads.clear();
-        soups.clear();
-        garnishes.clear();
-        mainCourses.clear();
-        appetizers.clear();
-        extras.clear();
-        vegMainCourses.clear();
+        dishes.clear();
+    }
+
+    public String toString() {
+        return "DailyMenu:" +
+                "day =" + day +
+                ", dishes =" + dishes + ".";
     }
 }
